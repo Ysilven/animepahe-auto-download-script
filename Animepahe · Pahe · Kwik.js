be55cd4d9507d032b673dbc4460dbaa973761fff
Arjien <88947398+Ysilven@users.noreply.github.com>
2023-11-29 06:50:11 +0800

    Animepahe · Pahe · Kwik.js
    new script implementation/optimization.

--- Animepahe · Pahe · Kwik.js
+++ Animepahe · Pahe · Kwik.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Animepahe · Pahe · Kwik
 // @namespace    https://PHCorner.net/
-// @version      0.0.2
+// @version      0.0.3
 // @downloadURL  https://raw.githubusercontent.com/Ysilven/animepahe-auto-download-script/main/Animepahe%20%C2%B7%20Pahe%20%C2%B7%20Kwik.js
 // @updateURL    https://raw.githubusercontent.com/Ysilven/animepahe-auto-download-script/main/Animepahe%20%C2%B7%20Pahe%20%C2%B7%20Kwik.js
 // @description  animepahe auto script. use mouse scroll click to open multiple anime links.
@@ -20,21 +20,18 @@
     'use strict';
 
     let settings = load_settings();
-    let enable·script = settings['Enable·Script'] ? true : false;
-
+    let enable_script = settings['Enable·Script'] ? true : false;
     let url_link = window.location.href;
     switch (true) {
         case /animepahe\.(ru|org|com)\/?$/.test(url_link):
-            menu(1);
-            break;
         case /animepahe\.(ru|org|com)(\/\?page=\d+)?$/.test(url_link):
             menu(1);
             break;
         case /animepahe\.(ru|org|com)\/play\/.+\/.+/.test(url_link):
             menu(1);
-            if(enable·script){
+            if(enable_script){
                 script();
-            }else{
+            } else {
                 console.log('script disabled');
             }
             break;
@@ -42,29 +39,44 @@
             menu(2);
             break;
         case /pahe\.win\/.+/.test(url_link):
-            pahe_win();
+            pahe_win(url_link);
             break;
         case /kwik\.cx\/f\/.+/.test(url_link):
             kwik_cx();
             break;
         case /kwik\.cx\/d\/.+/.test(url_link): {
-            let new_url_link = url_link.replace('/d/', '/f/');
-            window.location = new_url_link;
+            let newUrlLink = url_link.replace('/d/', '/f/');
+            window.location = newUrlLink;
             break;
         }
         default:
             console.log('no matches found for url_link');
     }
 
+    function clear_selection() {
+        let a = document.getElementById('pickDownload');
+        let b = a.getElementsByTagName('a');
+        for (let i = 0; i < b.length; i++) {
+            b[i].style.backgroundColor = '';
+            b[i].style.color = '';
+        }
+    }
+
     function script(){
         console.clear();
-        console.log('Animepahe · Pahe · Kwik', 'v0.0.2');
+        console.log('Animepahe · Pahe · Kwik', 'v0.0.3');
+
         let settings = load_settings();
         let index = settings['Resolution·Value'];
         let sub_dub = settings['Subtitle·Dubbed'] ? true : false;
         let resolution_checker = settings['Resolution·Checker'] ? true : false;
         let all_links = settings['All·Links'] ? true : false;
         let enable_script = settings['Enable·Script'] ? true : false;
+
+        let expand_menu = document.getElementById('downloadMenu');
+        if (expand_menu.getAttribute('aria-expanded') == 'false') {
+            expand_menu.click();
+        }
 
         let a = document.getElementById('pickDownload');
         let b = a.querySelectorAll('a');
@@ -75,6 +87,7 @@
                 text: link.textContent
             };
         });
+
         console.log('resolution·index:', index);
 
         let text = ['eng'];
@@ -98,7 +111,8 @@
                 }else{
                     console.log(`resolution·checker: ${resolutions[index]} · not found.`);
                     console.log('5 seconds reload.')
-                    setInterval(() => window.location.reload(), 5000);
+                    clear_selection();
+                    setTimeout(() => window.location.reload(), 5000);
                 }
             }
         }else{
@@ -106,42 +120,38 @@
         }
 
         function play(){
-            let expand_menu = document.getElementById('downloadMenu');
-            if (expand_menu.getAttribute('aria-expanded') == 'false') {
-                expand_menu.click();
-            }
-
             function filter(array, resolution) {
                 return array.filter(link => link.text.includes(resolution));
             }
 
             let count = sub_dub ? sub.length : dub.length;
-            if (!all_links){
+            if (!all_links) {
                 let type = sub_dub ? sub : dub;
                 let _720p = filter(type, '720p');
                 let _1080p = filter(type, '1080p');
-                if (count == 1 || index <= 0){
+
+                if (count == 1 || index <= 0) {
                     index = 1;
                 }
-                if (index == 2 & count == 2){
-                    if (_720p.length > 0){
+                if (index == 2 & count == 2) {
+                    if (_720p.length > 0) {
                         index = _720p[0].index;
-                    }else{
-                        if (_1080p.length > 0){
+                    } else {
+                        if (_1080p.length > 0) {
                             index = _1080p[0].index;
-                        }else{
+                        } else {
                             index = 1;
                         }
                     }
                 }
-                if (index == 3 & count == 2){
+                if (index == 3 & count == 2) {
                     index = 2;
                 }
-                if (index > count){
+                if (index > count) {
                     index = count;
                 }
-            }else{
-                if (index > links.length){
+            } else {
+                if (index > links.length) {
                     index = links.length;
                 }
             }
@@ -157,16 +167,6 @@
                 index_link = null;
             }
 
-            function clear_selection(){
-                let a = document.getElementById('pickDownload');
-                let b = a.getElementsByTagName('a');
-
-                for (let i = 0; i < b.length; i++) {
-                    b[i].style.backgroundColor = '';
-                    b[i].style.color = '';
-                }
-            }
-
             if (index_link == null){
                 console.log('resolution = value? · '+ index + ' · not found.');
                 clear_selection();
@@ -177,12 +177,8 @@
                 select.style.backgroundColor = '#505050';
                 select.style.color = 'white';
                 console.log(info);
-                if (enable_script){
-                    window.location = index_link;
-                    console.log(index_link);
-                }else{
-                    console.log('enable·script: false');
-                }
+
+                pahe_win(index_link);
             }
         }
     }
@@ -223,13 +219,16 @@
             if (!document.hidden) {
                 document.getElementById('settings_menu').remove();
                 create_menu();
-                if (load_settings()['Expand·Menu']) open_menu();
+                if (load_settings()['Expand·Menu']) {
+                    open_menu();
+                }
                 toggle(media);
                 media.addListener(toggle);
             }
         });
 
         function create_menu() {
+            let initialSettings = load_settings();
             let menu = document.createElement('div');
             let enable_script = document.createElement('button');
             let minus = document.createElement('button');
@@ -241,27 +240,13 @@
             let all_links = document.createElement('button');
             let expand_menu = document.createElement('button');
 
-            menu.id = 'settings_menu';
-            menu.style.display = 'none';
-            menu.style.backgroundColor = '#000';
-            menu.style.color = '#fff';
-            menu.style.minHeight = '10px';
-            menu.style.transition = 'all 0.3s ease'
-
             function settings(element, json_value, yes, no) {
                 let settings = load_settings();
                 element.innerText = settings[json_value] ? yes : no;
-
                 element.onclick = function() {
-                    let settings_ = load_settings();
-                    if (settings_[json_value] === false) {
-                        settings_[json_value] = true;
-                        element.innerText = yes;
-                    } else {
-                        settings_[json_value] = false;
-                        element.innerText = no;
-                    }
-                    save_settings(settings_);
+                    settings[json_value] = !settings[json_value];
+                    element.innerText = settings[json_value] ? yes : no;
+                    save_settings(settings);
                 };
             }
 
@@ -277,17 +262,24 @@
             plus.style.width = '35px';
             test_entry.innerText = '↻ Test Entry';
             input.readOnly = true;
-            let initialSettings = load_settings();
             input.value = initialSettings['Resolution·Value'];
             input.id = 'id_input';
             input.style.border = 'none';
-            input.style.background = '#252525';
+            input.style.background = '#202020';
+            input.style.borderRadius = '2px';
             input.style.color = '#FFF';
             input.style.width = '35px';
             input.style.height = '35px';
             input.style.textAlign = 'center';
             input.style.fontSize = '12px';
             input.style.margin = '2px 2px 0px -3px';
+
+            menu.id = 'settings_menu';
+            menu.style.display = 'none';
+            menu.style.backgroundColor = '#000';
+            menu.style.color = '#fff';
+            menu.style.minHeight = '10px';
+            menu.style.transition = 'all 0.3s ease'
 
             menu.appendChild(enable_script);
             menu.appendChild(minus);
@@ -335,11 +327,16 @@
                 let initialSettings = load_settings();
                 if (initialSettings['All·Links']) {
                     menu.removeChild(subtitle_dubbed);
-                    if (menu.contains(resolution_checker)) menu.removeChild(resolution_checker);
+                    if (menu.contains(resolution_checker)) {
+                        menu.removeChild(resolution_checker);
+                    }
                 } else {
                     menu.insertBefore(subtitle_dubbed, menu.children[5]);
-                    if (Number(input.value) <= 3) menu.insertBefore(resolution_checker, menu.children[6]);
-                    else if (menu.contains(resolution_checker)) menu.removeChild(resolution_checker);
+                    if (Number(input.value) <= 3) {
+                        menu.insertBefore(resolution_checker, menu.children[6]);
+                    } else if (menu.contains(resolution_checker)) {
+                        menu.removeChild(resolution_checker);
+                    }
                 }
             });
 
@@ -353,18 +350,27 @@
             });
 
             let all·links = initialSettings['All·Links'] ? true : false;
-            let resolution_value = initialSettings['Resolution·Value'] > 3;
-            if (!resolution_value) menu.insertBefore(resolution_checker, menu.children[6]);
+            let resolution·value = initialSettings['Resolution·Value'] > 3;
+            if (!resolution·value) {
+                menu.insertBefore(resolution_checker, menu.children[6]);
+            }
 
-            if (Number(input.value) > 3 && menu.contains(resolution_checker)) menu.removeChild(resolution_checker);
-            else if (!resolution_value) menu.insertBefore(resolution_checker, menu.children[6]);
+            if (Number(input.value) > 3 && menu.contains(resolution_checker)) {
+                menu.removeChild(resolution_checker);
+            } else if (!resolution·value) {
+                menu.insertBefore(resolution_checker, menu.children[6]);
+            }
 
             if (all·links) {
                 menu.removeChild(subtitle_dubbed);
-                if (menu.contains(resolution_checker)) menu.removeChild(resolution_checker);
+                if (menu.contains(resolution_checker)) {
+                    menu.removeChild(resolution_checker);
+                }
             } else {
                 menu.insertBefore(subtitle_dubbed, menu.children[5]);
-                if (!resolution_value) menu.insertBefore(resolution_checker, menu.children[6]);
+                if (!resolution·value) {
+                    menu.insertBefore(resolution_checker, menu.children[6]);
+                }
             }
 
             let main = document.querySelector('.content-wrapper');
@@ -380,10 +386,10 @@
 
             let buttons = [enable_script, minus, plus, test_entry, subtitle_dubbed, resolution_checker, all_links, expand_menu];
             for (let button of buttons) {
-                button.style.backgroundColor = '#000';
+                button.style.backgroundColor = '#101010';
                 button.style.color = '#fff';
-                button.style.border = '1px solid #353535';
-                button.style.borderRadius = '0px';
+                button.style.border = 'none';
+                button.style.borderRadius = '2px';
                 button.style.margin = '0px 5px 5px 0px';
                 button.style.padding = '0px 10px 0px 10px';
                 button.style.cursor = 'pointer';
@@ -392,10 +398,16 @@
                 button.style.fontSize = '12px';
                 button.style.height = '35px';
                 button.onmouseover = function() {
-                    this.style.backgroundColor = '#353535';
+                    this.style.backgroundColor = '#252525';
                 };
                 button.onmouseout = function() {
-                    this.style.backgroundColor = '#000';
+                    this.style.backgroundColor = '#101010';
+                };
+                button.onmousedown = function() {
+                    this.style.backgroundColor = '#1a1a1a';
+                };
+                button.onmouseup = function() {
+                    this.style.backgroundColor = '#252525';
                 };
             }
         }
@@ -436,18 +448,50 @@
     }
 
     function save_settings(settings) {
-        console.log('saving settings: ', settings);
+        console.log('saving settings');
         localStorage.setItem('settings', JSON.stringify(settings));
     }
 
-    function pahe_win() {
-        const content = document.querySelector('script').textContent;
-        const link = content.match(/https?:\/\/[^'"]+/);
-        if (link) {
-            window.location = link[0];
-        } else {
+    async function process_link(link){
+        let url = link;
+        let urls = [];
+        try {
+            let response = await fetch(url);
+            let html = await response.text();
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(html, 'text/html');
+            let links = doc.getElementsByTagName('a');
+            for(let i = 0; i < links.length; i++) {
+                let url = links[i].href;
+                if(url.includes('kwik.cx')) {
+                    urls.push(url);
+                }
+            }
+        } catch(err) {
             setInterval(() => window.location.reload(), 3000);
         }
+        console.log(urls[0]);
+        return urls;
+    }
+
+    let isProcessing = false;
+    function pahe_win(link) {
+        if (isProcessing) return;
+        isProcessing = true;
+        process_link(link).then(urls => {
+            isProcessing = false;
+            if(urls.length > 0) {
+                let settings = load_settings();
+                let enable_script = settings['Enable·Script'] ? true : false;
+                if (enable_script){
+                    window.location = urls[0];
+                }else{
+                    console.log('enable·script: false');
+                }
+            } else {
+                setInterval(() => window.location.reload(), 3000);
+            }
+        });
     }
 
     function kwik_cx() {
